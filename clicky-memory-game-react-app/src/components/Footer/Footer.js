fb262833f1373ff7c36d932ff1fa0859ca9b41db
Dheepa Sivakumaran <dheepasivakumaran@Dheepas-MacBook-Pro.local>
2018-10-17 12:20:11 -0400

    Add images and background files

---
+++ clicky-memory-game-react-app/src/components/Footer/Footer.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => <div className="footer text-center">Dheepa Sivakumaran</div>;
+
+export default Footer;
---
+++ clicky-memory-game-react-app/src/images.js
@@ -0,0 +1,29 @@
+import autumn from './images/autumn.jpeg';
+import blueflower from './images/blue-flower.jpeg';
+import blurredlight from './images/blurred-light.jpeg';
+import cold from './images/cold.jpeg';
+import lonelytree from './images/lonely-tree.jpeg';
+import lotus from './images/lotus.jpeg';
+import mapleleaf from './images/maple-leaf.jpeg';
+import grapeclusters from './images/grapeclusters.jpeg';
+import railroad from './images/railroad.jpeg';
+import red from './images/red.jpeg';
+import swans from './images/swans.jpeg';
+import waterfall from './images/waterfall.jpeg';
+
+const images = [
+  autumn,
+  blueflower,
+  blurredlight,
+  cold,
+  lonelytree,
+  lotus,
+  mapleleaf,
+  grapeclusters,
+  railroad,
+  red,
+  swans,
+  waterfall
+];
+
+export default images;

76d3ce07620e03a44fe6cf3ea434cb3a3206b7e1
Dheepa Sivakumaran <dheepasivakumaran@Dheepas-MacBook-Pro.local>
2018-11-01 21:38:33 -0400

    Add change to .gitignore file

---
+++ build/precache-manifest.c6d62f7a6b5bcd5f5637d4f123fc66b5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ade707b6c6a0472a725076de9f151d2",
+    "url": "/static/media/waterfall.6ade707b.jpeg"
+  },
+  {
+    "revision": "8d5d85ba128a435609ea89bf10f4b1d0",
+    "url": "/static/media/swans.8d5d85ba.jpeg"
+  },
+  {
+    "revision": "9146e46e1733c7933377ec33a8010954",
+    "url": "/static/media/red.9146e46e.jpeg"
+  },
+  {
+    "revision": "e53af05a47b4fbf8784792ae856f32a4",
+    "url": "/static/media/railroad.e53af05a.jpeg"
+  },
+  {
+    "revision": "a43cc65222676033fdc1b1eedeb40d2c",
+    "url": "/static/media/grapeclusters.a43cc652.jpeg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bb5956646d62dfe9d87f",
+    "url": "/static/js/main.bb595664.chunk.js"
+  },
+  {
+    "revision": "12d29667ff91d1ee3a05",
+    "url": "/static/js/1.12d29667.chunk.js"
+  },
+  {
+    "revision": "bb5956646d62dfe9d87f",
+    "url": "/static/css/main.d86f24d2.chunk.css"
+  },
+  {
+    "revision": "8f9b7791bc868814fe4bc4b8247d2ef4",
+    "url": "/index.html"
+  }
+];
